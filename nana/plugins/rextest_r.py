15008c7b504c2a48b65abd44a576e386e7d5f4a0
ポキ <aman_a@aol.com>
2021-01-08 01:05:46 +0500

    Rextester
    - added rextester module

---
+++ nana/plugins/rextest_r.py
@@ -0,0 +1,61 @@
+from rextester_py import rexec_aio, rextester_aio
+from nana import app, AdminSettings, COMMAND_PREFIXES, edit_or_reply
+from pyrogram import filters
+
+__MODULE__ = "Rextester"
+__HELP__ = """
+Yet the Another Developer Plugin.
+
+──「 **Rextester** 」──
+-> `rex` (*lanuguage) (*code)
+Executes the give code with the given language
+
+-> **Currently Supported Languages**
+```
+|  ada |   bash   |   brainfuck   |   c   |
+|  c++ |  elixir  |    erlang     |   f#  |
+|  go  |  fortran |    haskell    |   d   |
+| java |  kotlin  |  objective-c  |   js  |
+| lua  |   mysql  |   postgresql  |  node |
+| lisp |   ocaml  | python3 - py3 |  php  |
+| perl |  oracle  |  python - py  |  perl |
+| nasm |  pascal  |     prolog    |  sql  |
+| ruby |  octave  |     scheme    |   r   |
+| tcl  |  scheme  |     scala     |  sql  |
+|swift |   tcl    |      vb
+```
+"""
+
+
+@app.on_message(
+    filters.user(AdminSettings) &
+    filters.command("rex", COMMAND_PREFIXES)
+)
+async def rex_tester(_, message):
+    try:
+        args = message.text.split(None, 2)
+        language = args[1]
+        code = args[2]
+    except IndexError:
+        await edit_or_reply(
+            message,
+            text="Format: `rex lang code`"
+        )
+        return
+    try:
+        output = await rexec_aio(language, code)
+        final = f"**Language:** `{language}`\n"
+        final += f"**Input:**\n`{code}`\n"
+        if output.results:
+            final += f"**Output:**\n`{output.results}`\n"
+        if output.warnings:
+            final += f"**Warning:**\n`{output.warnings}`\n"
+        if output.errors:
+            final += f"**Error:**\n`{output.errors}`\n"
+        status = output.stats.split(", ")
+        final += "**Status:**\n"
+        for x in status:
+            final += f"`{x}`\n"
+        await edit_or_reply(message, text=final)
+    except rextester_aio.UnknownLanguage:
+        await edit_or_reply(message, text="Wrong language!")
